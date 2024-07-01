421b605a4d41242bb8ad3459fc1744a0be69c2da
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-01 13:51:06 +0530

    Src Commit

--- src/core/config.js
+++ src/core/config.js
@@ -1,3 +1,12 @@
-const config = { idLength: 8 };
+const config = {
+	idLength: 8,
+	items: [
+		{ name: 'Milk' },
+		{ name: 'Biscuit' },
+		{ name: 'Chocolate' },
+		{ name: 'Butter' },
+
+	],
+};
 
 export default config;
