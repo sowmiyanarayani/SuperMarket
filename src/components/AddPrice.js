7e104255e681078f91ad561714dd2bed3574e336
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-02 18:02:55 +0530

    Deleted

--- src/components/AddPrice.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-import PriceManager from '../services/PriceManager';
-
-const AddPrice = (context) => PriceManager.itemPrices(context);
-
-export default AddPrice;
