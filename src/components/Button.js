576883b1b6b4dcb77ef77beaab69989e2f47a602
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-09-23 19:15:41 +0530

    Once customer details is saved the form will automatically reset.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,21 @@
 import { React } from 'react';
+
+const validate = (state) =>
+	state.name !== ''
+		&& state.mobileNumber !== ''
+		&& state.gender !== ''
+	;
+
 const Button = (context) => {
-	const { actions: { addCustomer }} = context;
+	const { actions: { addCustomer, reset }, state } = context;
 
 	return <div>
-		<button onClick={ () => addCustomer() }>
+		<button onClick={ () => (
+			validate(state)
+				? (addCustomer(), reset())
+				: alert('Please fill in all fields.')
+		) }
+		>
 			Save Customer
 		</button>
 	</div>;
--- src/core/actions.js
+++ src/core/actions.js
@@ -5,12 +5,20 @@ const setCustomerGender = ({ data }) => ({ gender: data });
 const setSelectItems = ({ data }) => ({ items: data });
 const addCustomer = (context) =>
 	({ customerDetails: CustomerManager.addCustomer(context) });
+const reset = () => ({
+	name: '',
+	mobileNo: '',
+	gender: '',
+	items: [],
+
+});
 const actions = {
 	setCustomerName,
 	setCustomerMobileNo,
 	setCustomerGender,
 	setSelectItems,
 	addCustomer,
+	reset,
 };
 
 export default actions;
